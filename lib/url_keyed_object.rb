f2350f1a94719b47105052ee58d42965569badfb
Matt Patterson <matt@reprocessed.org>
2012-02-23 19:22:06 +0100

    Add combined decode / validate well-formedness of URL key method

--- lib/url_keyed_object.rb
+++ lib/url_keyed_object.rb
@@ -11,14 +11,14 @@ module UrlKeyedObject
                          "m", "n", "p", "q", "r", "s", "v", "w", "x", "y", 
                          "z"]
     end
-    
+
     # generates a url key from a random 5-digit base 31 number, without checking its uniqueness
     def generate_unchecked_url_key(length = 5)
       (1..length).collect do
         key_encoding[rand(31)]
       end.join('')
     end
-    
+
     # Keeps generating new URL keys until the passed-in block returns true
     def generate_checked_url_key(length = 5)
       key = generate_unchecked_url_key(length)
@@ -27,15 +27,21 @@ module UrlKeyedObject
       end
       key
     end
-    
+
     # Validate the well-formedness of a URL key
     def well_formed_url_key?(url_key, length = 5)
       !url_key.match(Regexp.new("^[0-9abcdefghjkmnpqrsvwxyz]{#{length.to_i}}$")).nil?
     end
-    
+
     # decode a moderately dirty URL key
     def decode_url_key(url_key)
       url_key.downcase.tr('o', '0').tr('il', '1')
+    end
+
+    # decode and validate a key, returning the decoded key only if it's well-formed
+    def decode_well_formed_url_key(url_key, length = 5)
+      decoded_url_key = decode_url_key(url_key)
+      well_formed_url_key?(decoded_url_key, length) ? decoded_url_key : nil
     end
   end
 end
--- spec/url_keyed_object_spec.rb
+++ spec/url_keyed_object_spec.rb
@@ -4,30 +4,30 @@ describe UrlKeyedObject do
   it "should be able to generate a 5-character URL key" do
     UrlKeyedObject.generate_unchecked_url_key.should match(/[0-9abcdefghjkmnpqrsvwxyz]{5}/)
   end
-  
+
   it "should be able to generate an arbitrary-length URL key" do
     UrlKeyedObject.generate_unchecked_url_key(7).should match(/[0-9abcdefghjkmnpqrsvwxyz]{7}/)
   end
-  
+
   describe "decoding a moderately dirty URL key (case insensitivity, ambiguous character replacement)" do
     it "should be able to cope with O o 0 ambiguity" do
       UrlKeyedObject.decode_url_key('Oo0ab').should == '000ab'
     end
-    
+
     it "should be able to cope with 1 I i L l ambiguity" do
       UrlKeyedObject.decode_url_key('1IiLl').should == '11111'
     end
-    
+
     it "should be able to cope with uppercase" do
       UrlKeyedObject.decode_url_key('ABCDE').should == 'abcde'
     end
   end
-  
+
   describe "well-formedness of URL keys" do
     it "should be able to confirm that a URL key is well-formed" do
       UrlKeyedObject.well_formed_url_key?('0ab9d').should be_true
     end
-    
+
     it "should report that malformed URL keys are NOT well-formed" do
       ['DfZZZ', # upper case
        'abcdef', # too long
@@ -41,12 +41,12 @@ describe UrlKeyedObject do
         UrlKeyedObject.well_formed_url_key?(malformed_url_key).should be_false
       end
     end
-    
+
     it "should be able to cope with a different specified length" do
       UrlKeyedObject.well_formed_url_key?('abcdef', 6).should be_true
     end
   end
-  
+
   describe "validity of URL keys" do
     it "should be able to evaluate a block to check key validity, repeating until it generates one" do
       valid_key_sequence = sequence('valid key sequence')
@@ -54,14 +54,17 @@ describe UrlKeyedObject do
                      in_sequence(valid_key_sequence)
       UrlKeyedObject.expects(:generate_unchecked_url_key).with(5).returns('a2url').
                      in_sequence(valid_key_sequence)
-      
+
       key = UrlKeyedObject.generate_checked_url_key { |value| value != 'a1url' }
-      
+
       key.should == 'a2url'
     end
-    
-    it "should description" do
-      
-    end
+  end
+
+  it "can return a decoded key that has been checked for well-formedness" do
+    UrlKeyedObject.expects(:decode_url_key).with('ABCDE').returns('abcde')
+    UrlKeyedObject.expects(:well_formed_url_key?).with('abcde', 5).returns('abcde')
+
+    UrlKeyedObject.decode_well_formed_url_key('ABCDE').should == 'abcde'
   end
 end
